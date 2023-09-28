56c55eb42a64160bbd3619d7cda3b97eeec6139a
Gabriel <scapimgabriel@gmail.com>
2023-09-28 14:54:13 -0300

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import SideBar from './components/SideBar';
 import AppRoutes from './routes';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <main>
         <SideBar />
         <AppRoutes />
