9c0018d865f61957b81d0892743dad477aaf6e99
Gabriel <scapimgabriel@gmail.com>
2023-09-28 16:01:05 -0300

    styles: nav bar

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,12 +1,29 @@
+import { useEffect, useState } from 'react';
 import styles from './SideBar.module.css';
 
 function SideBar() {
-  const location: string = window.location.href;
+  const location = window.location.href;
   const navLocationStyle = {
     backgroundColor: '#e3d5eb',
   };
+  const displayNoneStyle = {
+    display: 'none',
+  };
 
-  console.log(location);
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [setWidth]);
 
   return (
     <aside className={ styles['side-bar'] }>
@@ -16,28 +33,36 @@ function SideBar() {
           style={ location.includes('agendamentos') ? navLocationStyle : {} }
         >
           <i className="bi bi-clock"> </i>
-          Agendamentos
+          <h5 style={ width <= 824 ? displayNoneStyle : {} }>
+            Agendamentos
+          </h5>
         </a>
         <a
           href="/"
           style={ location.includes('gerenciar-datas') ? navLocationStyle : {} }
         >
           <i className="bi bi-calendar"> </i>
-          Gerenciar datas
+          <h5 style={ width <= 824 ? displayNoneStyle : {} }>
+            Datas
+          </h5>
         </a>
         <a
           href="/"
           style={ location.includes('cliented') ? navLocationStyle : {} }
         >
           <i className="bi bi-person"> </i>
-          Clientes
+          <h5 style={ width <= 824 ? displayNoneStyle : {} }>
+            Clientes
+          </h5>
         </a>
         <a
           href="/"
           style={ location.includes('financeiro') ? navLocationStyle : {} }
         >
           <i className="bi bi-graph-up-arrow"> </i>
-          Financeiro
+          <h5 style={ width <= 824 ? displayNoneStyle : {} }>
+            Financeiro
+          </h5>
         </a>
       </nav>
     </aside>
