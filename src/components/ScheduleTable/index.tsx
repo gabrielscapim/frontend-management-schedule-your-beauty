f5dccc22b61fcc65d1cc9d79bbee3ead3bf75b4a
Gabriel <scapimgabriel@gmail.com>
2023-10-02 09:50:55 -0300

    feat: schedule table component

---
+++ src/components/ScheduleTable/index.tsx
@@ -0,0 +1,55 @@
+import Schedule from '../../types/Schedule';
+import styles from './ScheduleTable.module.css';
+
+interface ScheduleTableProps {
+  schedules: Schedule[];
+}
+
+function ScheduleTable({ schedules }: ScheduleTableProps) {
+  return (
+    <table className={ styles.table }>
+      <thead>
+        <tr>
+          <th className={ styles['first-column'] }>NOME</th>
+          <th>DATA</th>
+          <th>HORÁRIO</th>
+          <th>PRODUÇÃO</th>
+          <th>EVENTO</th>
+          <th> </th>
+          <th className={ styles['last-column'] }> </th>
+        </tr>
+      </thead>
+      <tbody>
+        { schedules.map((
+          { id,
+            client: { name: clientName },
+            date,
+            startTime,
+            production: { name: productionName },
+            eventName,
+          },
+        ) => (
+          <tr key={ `schedule-${id}` }>
+            <td style={ { fontWeight: 'bold' } }>{clientName}</td>
+            <td>{date}</td>
+            <td>{startTime}</td>
+            <td>{productionName}</td>
+            <td>{eventName}</td>
+            <td className={ styles['handle-column'] }>
+              <button style={ { color: '#765ac2', fontWeight: 'bold' } }>
+                Contato
+              </button>
+            </td>
+            <td className={ styles['handle-column'] }>
+              <button style={ { color: '#dc3545', fontWeight: 'bold' } }>
+                Cancelar
+              </button>
+            </td>
+          </tr>
+        )) }
+      </tbody>
+    </table>
+  );
+}
+
+export default ScheduleTable;
