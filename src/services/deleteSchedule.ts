e9bcbefd5c200f79a060e796a9716e0abeedc424
Gabriel <scapimgabriel@gmail.com>
2023-10-02 20:15:54 -0300

    fix: delete schedule

--- src/services/deleteSchedule.ts
+++ src/services/deleteSchedule.ts
@@ -1,7 +1,10 @@
 import apiRequest from './apiRequest';
 
 const deleteSchedule = async (id: number) => {
-  await apiRequest('delete', `/schedule/${id}`);
+  if (window.confirm('Deseja cancelar o agendamento?')) {
+    await apiRequest('delete', `/schedule/${id}`);
+    window.location.reload();
+  }
 };
 
 export default deleteSchedule;
