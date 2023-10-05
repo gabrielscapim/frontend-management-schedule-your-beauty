5c24ff16d1f9571b2a2f56b36d6096091e8b9bc9
Gabriel <scapimgabriel@gmail.com>
2023-10-05 16:08:34 -0300

    feat: post scheduling times request

---
+++ src/services/postSchedulingTimes.ts
@@ -0,0 +1,20 @@
+import getAvailableTimesSelected from '../helpers/getAvailableTimesSelected';
+import SchedulingTime from '../types/SchedulingTime';
+import apiRequest from './apiRequest';
+
+const postSchedulingTimes = async (
+  date: string,
+  state: any,
+  timesFromApi: SchedulingTime[],
+): Promise<void> => {
+  const { lastTimeToSchedule } = state;
+  const availableTimesSelected = getAvailableTimesSelected(state, timesFromApi);
+  const apiObject = {
+    times: availableTimesSelected,
+    ...(lastTimeToSchedule !== 'Nenhum horário' && { lastTimeToSchedule }),
+  };
+
+  await apiRequest('post', `/scheduling-date-time/time/${date}`, apiObject);
+};
+
+export default postSchedulingTimes;
