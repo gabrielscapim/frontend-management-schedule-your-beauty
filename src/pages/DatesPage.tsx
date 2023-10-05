71ad1d48b8ceb2d1bd1c71a2909d226d0ac83c47
Gabriel <scapimgabriel@gmail.com>
2023-10-05 16:08:48 -0300

    feat: post scheduling times on page

--- src/pages/DatesPage.tsx
+++ src/pages/DatesPage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 import { useEffect, useState } from 'react';
 import moment from 'moment';
 import Checkbox from '../components/Checkbox';
@@ -8,38 +9,59 @@ import fetchSchedulingTimes from '../services/fetchSchedulingTimes';
 import SchedulingTime from '../types/SchedulingTime';
 import filterSchedulingTimes from '../helpers/filterSchedulingTimes';
 import Loading from '../components/Loading';
+import Select from '../components/Select';
+import Button from '../components/Button';
+import getAvailableTimesSelected from '../helpers/getAvailableTimesSelected';
+import postSchedulingTimes from '../services/postSchedulingTimes';
 
 function DatesPage() {
   const CURRENT_DATE = moment().format('YYYY-MM-DD');
 
-  const [timesFromApi, setTimesFromApi] = useState<SchedulingTime[] | []>([]);
+  const [timesFromApi, setTimesFromApi] = useState<SchedulingTime[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [state, setState] = useState<any>({ scheduleDate: CURRENT_DATE });
-  const [dateSearched, setDateSearched] = useState<string>(state.scheduleDate);
+  const [state, setState] = useState<any>({
+    date: CURRENT_DATE,
+    lastTimeToSchedule: 'Nenhum horário',
+  });
+  const [dateSearched, setDateSearched] = useState<string>(state.date);
 
   const getSchedulingTimes = async () => {
     setIsLoading(true);
-    setDateSearched(state.scheduleDate);
-    const schedulingTimes = await fetchSchedulingTimes(state.scheduleDate);
+    setDateSearched(state.date);
+    const schedulingTimes = await fetchSchedulingTimes(state.date);
     const filteredSchedulingTimes = filterSchedulingTimes(schedulingTimes);
+    const lastTimeToSchedule = schedulingTimes
+      .find((time) => time.lastScheduleTimeDay === true)?.time;
+    console.log(lastTimeToSchedule);
 
     setTimesFromApi(schedulingTimes);
     setState((prevState: any) => ({
       ...prevState,
+      lastTimeToSchedule: lastTimeToSchedule || 'Nenhum horário',
       ...allTimes,
       ...filteredSchedulingTimes }));
     setIsLoading(false);
+  };
+
+  const handleUpdateTimes = async () => {
+    if (window.confirm('Deseja atualizar os horários de agendamento?')) {
+      await postSchedulingTimes(dateSearched, state, timesFromApi);
+      window.alert('Horários atualizados com sucesso!');
+      window.location.reload();
+    }
   };
 
   useEffect(() => {
     getSchedulingTimes();
   }, []);
 
-  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value, checked, type } = target;
+  const handleChange = ({ target }:
+  React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value, type } = target;
     return setState((prevState: any) => ({
       ...prevState,
-      [name]: type === 'checkbox' ? checked : value,
+      [name]: type === 'checkbox' && target instanceof HTMLInputElement
+        ? target.checked : value,
     }));
   };
 
@@ -52,10 +74,10 @@ function DatesPage() {
       <div className={ styles['search-container'] }>
         <Input
           type="date"
-          id="scheduleDate"
+          id="date"
           label=""
-          name="scheduleDate"
-          value={ state.scheduleDate }
+          name="date"
+          value={ state.date }
           handleChange={ handleChange }
         />
         <button
@@ -67,24 +89,53 @@ function DatesPage() {
       </div>
       { isLoading && <Loading /> }
       { !isLoading && (
-        <div className={ styles['times-container'] }>
-          <h6>Selecione os horários que você pode realizar uma produção.</h6>
-          <div className={ styles['checkbox-container'] }>
-            { Object.keys(allTimes).map((time) => (
-              <Checkbox
-                key={ time }
-                id={ time }
-                label={ time }
-                handleChange={ handleChange }
-                name={ time }
-                disabled={ timesFromApi.some((timeFromApi) => (
-                  timeFromApi.time === time && !timeFromApi.available
-                )) }
-                checked={ state[time as keyof typeof allTimes] }
-              />
-            )) }
+        <>
+          <div className={ styles['times-container'] }>
+            <h6>Selecione os horários que você pode realizar uma produção</h6>
+            <div className={ styles['checkbox-container'] }>
+              { Object.keys(allTimes).map((time) => (
+                <Checkbox
+                  key={ time }
+                  id={ time }
+                  label={ time }
+                  handleChange={ handleChange }
+                  name={ time }
+                  disabled={ timesFromApi.some((timeFromApi) => (
+                    timeFromApi.time === time && !timeFromApi.available
+                  )) }
+                  checked={ state[time as keyof typeof allTimes] }
+                />
+              )) }
+            </div>
+            { timesFromApi.some((time) => time.lastScheduleTimeDay === true
+            && !time.available) ? (
+              <span>
+                { `
+                Último horário do dia (sem disponibilidade): ${
+                  timesFromApi.find((time) => time.lastScheduleTimeDay === true)?.time} 
+                  Hrs` }
+              </span>
+              ) : (
+                <Select
+                  id="lastTimeToSchedule"
+                  label="Último horário disponível para agendamento"
+                  handleChange={ handleChange }
+                  options={ ['Nenhum horário',
+                    getAvailableTimesSelected(state, timesFromApi).slice(-1)[0]] }
+                  name="lastTimeToSchedule"
+                  disabled={ (Object
+                    .keys(state).filter((key) => (state[key] === true))).length === 0 }
+                  value={ state.lastTimeToSchedule }
+                />
+              ) }
           </div>
-        </div>
+          <div className={ styles['button-container'] }>
+            <Button
+              label="Atualizar horários"
+              handleClick={ handleUpdateTimes }
+            />
+          </div>
+        </>
       ) }
     </section>
   );
