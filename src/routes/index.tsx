b1bab015bab64219f3d349c91fb79346c82399f8
Gabriel <scapimgabriel@gmail.com>
2023-10-05 16:51:15 -0300

    feat: main page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ManagerSchedulesPage from '../pages/ManagerSchedulesPage';
 import DatesPage from '../pages/DatesPage';
 import NotFoundPage from '../pages/NotFoundPage';
+import MainPage from '../pages/MainPage';
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={ <ManagerSchedulesPage /> } /> */}
+        <Route path="/" element={ <MainPage /> } />
         <Route path="/agendamentos" element={ <ManagerSchedulesPage /> } />
         <Route path="/datas" element={ <DatesPage /> } />
         <Route path="*" element={ <NotFoundPage /> } />
