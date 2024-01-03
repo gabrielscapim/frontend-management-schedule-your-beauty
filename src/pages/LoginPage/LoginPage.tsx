e927bf5d517e8c4025ca55911f7d0ba779441d15
Gabriel Scapim <scapimgabriel@gmail.com>
2024-01-03 08:24:00 -0300

    feature: create login and password inputs

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,8 +1,24 @@
+import Input from '../../components/Input';
 import styles from './LoginPage.module.css';
 
 function LoginPage() {
   return (
-    <main className={ styles['page-container'] } />
+    <main className={ styles['page-container'] }>
+      <section>
+        <Input
+          id="login-input"
+          label="Login"
+          placeholder="Digite seu login"
+          handleChange={ () => {} }
+        />
+        <Input
+          id="password-input"
+          label="Senha"
+          placeholder="Digite sua senha"
+          handleChange={ () => {} }
+        />
+      </section>
+    </main>
   );
 }
 
