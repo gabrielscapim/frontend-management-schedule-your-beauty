8c81dc3b0b6492c464bf54623cc84904391a3ced
Gabriel Scapim <scapimgabriel@gmail.com>
2024-01-03 08:32:12 -0300

    fix: add return for login route component

--- src/routes/LoginRoute/index.tsx
+++ src/routes/LoginRoute/index.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from '../../pages/LoginPage/LoginPage';
 
 function LoginRoute() {
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={ <LoginPage /> } />
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <LoginPage /> } />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default LoginRoute;
