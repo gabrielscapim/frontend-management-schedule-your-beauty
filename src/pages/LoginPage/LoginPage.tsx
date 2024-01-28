6758dba8f9908460b4fd8edc1cdd548980658636
Gabriel Scapim <scapimgabriel@gmail.com>
2024-01-27 21:27:07 -0300

    Create Loginpage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,26 +1,47 @@
+import { useState } from 'react';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import styles from './LoginPage.module.css';
+import login from '../../services/login';
 
 function LoginPage() {
+  const [state, setState] = useState({
+    username: '',
+    password: '',
+  });
+  const { username, password } = state;
+
+  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = target;
+    return setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   return (
     <main className={ styles['page-container'] }>
-      <section>
+      <section className={ styles['login-container'] }>
         <Input
           id="login-input"
           label="Login"
           placeholder="Digite seu login"
-          handleChange={ () => {} }
+          name="username"
+          value={ username }
+          handleChange={ handleChange }
         />
         <Input
           id="password-input"
           label="Senha"
+          type="password"
+          name="password"
           placeholder="Digite sua senha"
-          handleChange={ () => {} }
+          value={ password }
+          handleChange={ handleChange }
         />
         <Button
           label="Entrar"
-          handleClick={ () => {} }
+          handleClick={ async () => { await login(username, password); } }
         />
       </section>
     </main>
